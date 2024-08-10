48addc47600c3e722f934ddd151317029ad00663
Pedro Matos <pedrojmatos94@gmail.com>
2024-08-09 22:51:52 -0300

    style(view): about view text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,7 +6,7 @@ import DefaultLayout from '../layouts/DefaultLayout.vue'
   <DefaultLayout>
     <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
       <div>
-        <div class="text-xl font-medium text-black">About</div>
+        <div class="text-xl font-medium text-black">About View</div>
       </div>
     </div>
   </DefaultLayout>
